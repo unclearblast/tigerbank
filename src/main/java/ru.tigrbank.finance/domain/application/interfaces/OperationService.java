919d09a29b319c3d4f9a09b05fa640f64679fe46
unclearblast <derenikhm@gmail.com>
2026-02-08 00:16:31 +0300

    Create OperationService.java

---
+++ src/main/java/ru.tigrbank.finance/domain/application/interfaces/OperationService.java
@@ -0,0 +1,7 @@
+package ru.tigrbank.finance.application.interfaces;
+
+import ru.tigrbank.finance.domain.entity.Operation;
+
+public interface OperationService {
+    void add(Operation operation);
+}
